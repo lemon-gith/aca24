59fceb1b3337e4582677a5f564f92d01279eebb5
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-02-06 23:10:12 +0000

    update script readability

--- testbash.sh
+++ testbash.sh
@@ -1,10 +1,13 @@
 #! /usr/bin/bash
 
 scripts_array=("varyarch-energy-3params" "varycachelinesize" "varycachesize")
+arr_last_idx=$((${#scripts_array[@]} - 1))
 
-for i in 0 1 2
+# use indices to be able to iterate through destinations, too
+for i in $(seq 0 $arr_last_idx)
 do 
-	ssh texel"($i+15)" "cd $TEST_PATH; ./scripts/${scripts_array[$i]} >> ./remote_testing/next-${scripts_array[$i]}.txt; exit;" &
+	script=${scripts_array[$i]}
+	ssh texel$(($i+5)) "cd $TEST_PATH; ./scripts/$script >> ./remote_testing/next-$script.txt; exit;" &
 done
 
 echo "greetings, I am returned"
